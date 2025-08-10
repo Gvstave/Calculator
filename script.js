5504da69b696fd5eebdc2c2a7199d98b1e9b6c6d
Gvstave <ilungagustave73@gmail.com>
2025-08-10 20:54:54 +0200

    refactor code and redesign the app

--- script.js
+++ script.js
@@ -1,81 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
   const input = document.querySelector("#input");
-
   const result = document.querySelector("#result");
-
   const info = document.querySelector("#info");
-
   const bracket = document.querySelector("#brackets");
-
-  const percent = document.querySelector("#percent");
-
   const btn = document.querySelectorAll(".btn");
-
   const clear = document.querySelector("#clear");
-
   const equal = document.querySelector("#equal");
-  
-  /*function for detecting button click and
-   writing button click to the input field*/
-  function buttonFunc() {
-    for (let i = 0; i < btn.length; i++) {
-      btn[i].querySelectorAll(".btn");
-      btn[i].addEventListener("click", function () {
-        //alert(btn[i].textContent)
-        input.value += btn[i].textContent;
-        calculate();
-      });
-    }
-  }
-  buttonFunc();
 
-  //function for calculating/evaluating the input
+  // Add event listener to all the buttons with the class btn.
+  btn.forEach(button => {
+    button.addEventListener("click", function () {
+      input.value += button.value;
+      calculate();
+    });
+  });
+
   function calculate() {
-
-    const output = eval(input.value);
+    let output = eval(input.value);
     result.value = output;
 
-    //These buttons have a special pu
-    bracket.addEventListener("click", function () {
-      input.value = `(${output})*`;
-    });
-
-    percent.addEventListener("click", function () {
-      input.value = `${output}/100`;
-      result.value = `${output/100}`;
-    });
-
-    equal.addEventListener("click", function () {
-      input.value = output.toFixed(2);
-      result.value = "";
-    });
-
     try {
-      if (result.value == "NaN") {
-        throw new SyntaxError("Not a number");
-        
-      } else if (result.value == "Infinity") {
-        throw new SyntaxError("Can't divide by 0");
-        
-      }
-      else if(result.value == "undefined"){
-        throw new SyntaxError("Invalid input");
-      }
+      if (isNaN(output)) throw new SyntaxError("Not a number");
+      if (!isFinite(output)) throw new SyntaxError("Can't divide by 0");
+      if (result.value == "undefined") throw new SyntaxError("Invalid input");
     } catch (e) {
-      info.value = `Error: ${e.message}`;
+      info.value = `${e.message}`;
       info.style.color = 'red'
+      result.value = 0;
     }
   }
 
-  //function to clear the the input fields
-  function clean() {
-    clear.addEventListener("click", function () {
-      input.value = "";
+  bracket.addEventListener("click", function () {
+    input.value = `(${result.value || 0})*`;
+  });
+
+  equal.addEventListener("click", function () {
+    if (!Number.isInteger(input.value)) {
+      // let fixed = input.value;
+      input.value = result.value || input.value;
       result.value = "";
-      info.value = "";
-      info.style.color = 'gray'
-    });
-  }
-  clean();
+    } else {
+      input.value = result.value || input.value;
+      result.value = 0;
+    }
+    info.value = ""
+  });
+
+  clear.addEventListener("click", function () {
+    input.value = "";
+    result.value = "";
+    info.value = "";
+    info.style.color = 'gray'
+  });
 
 });
